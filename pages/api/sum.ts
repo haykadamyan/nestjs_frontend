0a5c043ff5ca67a8827aa379e2ce8638b0bf9f54
Hayk Adamyan <haykadamyan@Hayks-MacBook-Pro.local>
2022-03-21 15:47:20 +0400

    chore: update http method

--- pages/api/sum.ts
+++ pages/api/sum.ts
@@ -1,7 +1,10 @@
 import fetch from "lib/fetch"
 
 export default async function getSum(a: string, b: string) {
-    const res = await fetch(`/sum/${a}/${b}`, {})
+    const res = await fetch(`/sum`, {
+        method: 'POST',
+        data: { a, b }
+    })
 
     return res;
 }
